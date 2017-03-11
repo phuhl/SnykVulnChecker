73199aeb794e2dcb32906d8b6bfcbe55edf85b85
Philipp Uhl <philipp.uhl@rwth-aachen.de>
2017-03-11 18:28:03 +0100

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,107 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Lib
+    ( run
+    ) where
+
+
+import GHC.Generics
+
+import Data.Aeson -- (parseJSON, fromJSON, Value, Result, ToJSONKey)
+import Data.Aeson.Types
+import Data.Aeson.Parser -- (decodeWith, json)
+import Data.Aeson.Internal (IResult,ifromJSON)
+import Text.Feed.Import (parseFeedFromFile)
+import qualified Data.HashMap.Strict as HM
+import Text.Feed.Types (Feed (RSSFeed))
+import Text.RSS.Syntax as RSS
+import Text.Regex.PCRE ((=~))
+import HTMLEntities.Decoder (htmlEncodedText)
+import Data.Text.Lazy.Builder (toLazyText)
+import Data.Text (pack)
+import Data.Text.Lazy (unpack)
+import qualified Data.ByteString.Lazy as BSL
+import Data.Either.Utils
+
+
+type Dependencies =
+  [(String, String)]
+
+data Vul = Vul
+  { vulTitle :: String
+  , vulPackage :: String
+  , vulVersion :: String
+  , vulSeverity :: String
+  , vulUrl :: String
+  , vulDate :: DateString
+  , vulDescription :: String
+  } deriving (Show)
+
+run :: IO ()
+run = do
+  feed <- parseFeedFromFile "./feed.xml"
+  putStrLn "[OK] feed.xml read"
+  packageJSON <- BSL.readFile "package.json"
+  putStrLn "[OK] package.json read"
+  let res = do
+        let allItems = case feed of
+              RSSFeed (RSS.RSS _ _ channel _) -> rssItems channel
+        let items = filter checkForNpm allItems
+        Right $ putStrLn $ "[OK] " ++ (show $ length items) ++ " Vulnerabilities found in feed.xml"
+        let vuls = map fromRegex
+                   $ zip3 (fromTitle items) (fromDate items) (fromDescription items) :: [Vul]
+        Right $ putStrLn $ "[OK] Parsed feed.xml"
+        r <- liftLeft $ eitherDecode packageJSON
+        let lrDeps = parsePackageJSON r
+        deps <- liftEither $ lrDeps :: Either (IO String) (IO Dependencies)
+        Right $ putStrLn <$> show <$> deps
+        Right $ show <$> deps
+
+  case res of
+    Left ioe -> do e <- ioe
+                   putStrLn $ "[ERROR] " ++ (show e)
+    Right ior -> do r <- ior
+                    putStrLn $ "[OK] " ++ (show r)
+
+
+checkForNpm :: RSSItem -> Bool
+checkForNpm i = maybe False
+                (\guid -> "npm:" == (take 4 $ rssGuidValue guid))
+                (rssItemGuid i)
+
+fromRegex :: ((String, String, String, [String]), DateString, String) -> Vul
+fromRegex ((_, _, _, (a:b:c:d:e:[])), f, g) = Vul a b c d e f g
+
+
+fromTitle = map (\t -> t =~ "^(.*)\\sfor\\s(.*)\\s(.*)\\swith\\s(medium|high|low)\\sseverity\\s(.*)$")
+            . map (replaceSpace)
+            . map (unpack . toLazyText . htmlEncodedText . pack)
+            . map (unpack . toLazyText . htmlEncodedText . pack)
+            . map (maybe "" id)
+            . map rssItemTitle
+              where
+                replaceSpace = map (\s -> if s == '\160' then ' ' else s)
+
+fromDate = map (maybe "" id)
+           . map rssItemPubDate
+fromDescription = map (maybe "" id)
+                  . map rssItemDescription
+
+parsePackageJSON :: Object -> Either String Dependencies
+parsePackageJSON p = do
+  flip parseEither p $ \obj -> do
+    dep <- obj .: (pack "dependencies")
+    maybe [] HM.toList <$> parseJSON dep
+
+
+liftEither :: Either a b -> Either (IO a) (IO b)
+liftEither (Left a) = Left $ return a
+liftEither (Right a) = Right $ return a
+
+liftLeft :: Either a b -> Either (IO a) b
+liftLeft (Left a) = Left $ return a
+liftLeft (Right a) = Right a
+
+liftRight :: Either a b -> Either a (IO b)
+liftRight (Right a) = Right $ return a
+liftRight (Left a) = Left a
